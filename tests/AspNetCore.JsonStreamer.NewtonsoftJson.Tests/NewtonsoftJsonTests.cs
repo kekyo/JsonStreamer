9eeb703c63be22842800f692f8c8b65ba75d1e44
Kouji Matsui <k@kekyo.net>
2023-09-21 10:24:22 +0900

    Improved test code.

--- tests/AspNetCore.JsonStreamer.NewtonsoftJson.Tests/NewtonsoftJsonTests.cs
+++ tests/AspNetCore.JsonStreamer.NewtonsoftJson.Tests/NewtonsoftJsonTests.cs
@@ -14,7 +14,11 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AspNetCore.JsonStreamer;
@@ -23,29 +27,48 @@ public sealed class NewtonsoftJsonTests
 {
     private static HttpClient httpClient = new();
 
-    private async IAsyncEnumerable<T> StreamingFetchAsync<T>(Uri url)
+    private async IAsyncEnumerable<T> StreamingFetchAsync<T>(
+        Uri url, [EnumeratorCancellation] CancellationToken ct)
     {
         using var response = await httpClient.GetAsync(
-           url,
-           HttpCompletionOption.ResponseHeadersRead);
+           url, HttpCompletionOption.ResponseHeadersRead, ct);
 
         response.EnsureSuccessStatusCode();
 
-        await using var stream = await response.Content.ReadAsStreamAsync();
-        var tr = new StreamReader(stream);
+        var encoding = response.Content.Headers.ContentEncoding.
+            Select(Encoding.GetEncoding).
+            Where(e => e != null).
+            FirstOrDefault() ?? Encoding.UTF8;
 
-        while (true)
+        await using var stream = await response.Content.ReadAsStreamAsync(ct);
+        var tr = new StreamReader(stream, encoding, true);
+
+        var readLineTask = tr.ReadLineAsync().WaitAsync(ct);
+        try
         {
-            var line = await tr.ReadLineAsync();
-            if (line == null)
+            while (true)
             {
-                break;
+                var line = await readLineTask;
+                readLineTask = null;
+                if (line == null)
+                {
+                    break;
+                }
+
+                readLineTask = tr.ReadLineAsync().WaitAsync(ct);
+
+                var jt = JToken.Parse(line);
+                var item = jt.ToObject<T>()!;
+
+                yield return item;
             }
-
-            var jt = JToken.Parse(line);
-            var item = jt.ToObject<T>()!;
-
-            yield return item;
+        }
+        finally
+        {
+            if (readLineTask != null)
+            {
+                readLineTask.Dispose();
+            }
         }
     }
 
@@ -62,7 +85,7 @@ public sealed class NewtonsoftJsonTests
 
         var count = 0;
         await foreach (var item in
-            this.StreamingFetchAsync<TestModel>(testWebApplication.Url))
+            this.StreamingFetchAsync<TestModel>(testWebApplication.Url, default))
         {
             Assert.AreEqual(count, item.Index);
 
